bf8ed45bf21aef8e839332d3cd61f32017aef040
Yaseen <121yaseen@gmail.com>
2024-12-23 17:58:37 +0530

    fix dashboard bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,6 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function AppHome() {
-  const { status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/login"); // Redirect to login if not authenticated
-    } else if (status === "authenticated") {
-      router.push("/dashboard"); // Redirect to dashboard if authenticated
-    }
-  }, [status, router]);
-
-  if (status === "loading") {
-    return <div>Loading...</div>; // Optionally add a loading spinner
-  }
-
+  redirect("/dashboard");
   return null;
 }
