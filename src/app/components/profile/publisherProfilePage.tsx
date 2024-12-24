5c11b5619c8fdf41a3b47b692e5b2c4bd882c63f
Yaseen <121yaseen@gmail.com>
2024-12-24 17:33:38 +0530

    build fix

--- src/app/components/profile/publisherProfilePage.tsx
+++ src/app/components/profile/publisherProfilePage.tsx
@@ -8,6 +8,8 @@ import Image from "next/image";
 
 const PublisherProfilePage: React.FC = () => {
   const { data: session } = useSession();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [userProfile, setUserProfile] = useState(null);
   const [formData, setFormData] = useState<{
     profilePicUrl: string | File;
