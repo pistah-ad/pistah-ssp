c975de680bc6c6ceb91e10f15ed5868aaa9ca076
Yaseen <121yaseen@gmail.com>
2024-12-28 03:07:16 +0530

    add reload and toast in profile page

--- src/app/components/profile/publisherProfilePage.tsx
+++ src/app/components/profile/publisherProfilePage.tsx
@@ -5,14 +5,16 @@ import PencilIcon from "@/icons/pencilIcon";
 import ProfileIcon from "@/icons/profileIcon";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import Loader from "../shared/LoaderComponent";
+import { add, set } from "date-fns";
 import { useToast } from "@/app/context/ToastContext";
 
 const PublisherProfilePage: React.FC = () => {
   const { data: session } = useSession();
   const { addToast } = useToast();
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [userProfile, setUserProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState<{
     profilePicUrl: string | File;
     name: string;
@@ -26,31 +28,31 @@ const PublisherProfilePage: React.FC = () => {
   });
   const [preview, setPreview] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      if (session?.user?.email) {
-        try {
-          const response = await fetch(`/api/user/${session.user.email}`);
-          if (response.ok) {
-            const data = await response.json();
-            setUserProfile(data);
-            setFormData({
-              profilePicUrl: data.profilePicUrl || "",
-              name: data.name || "",
-              email: data.email || "",
-              companyName: data.Company?.name || "",
-            });
-          } else {
-            addToast("Something went wrong!", "error");
-            console.error("Failed to fetch user profile");
-          }
-        } catch (error) {
-          addToast("Something went wrong!", "error");
-          console.error("Error fetching user profile:", error);
+  const fetchUserProfile = async () => {
+    setLoading(true);
+    if (session?.user?.email) {
+      try {
+        const response = await fetch(`/api/user/${session.user.email}`);
+        if (response.ok) {
+          const data = await response.json();
+          setUserProfile(data);
+          setFormData({
+            profilePicUrl: data.profilePicUrl || "",
+            name: data.name || "",
+            email: data.email || "",
+            companyName: data.Company?.name || "",
+          });
+        } else {
+          console.error("Failed to fetch user profile");
         }
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+      } finally {
+        setLoading(false);
       }
-    };
-
+    }
+  };
+  useEffect(() => {
     fetchUserProfile();
   }, [session]);
 
@@ -79,6 +81,7 @@ const PublisherProfilePage: React.FC = () => {
     e.preventDefault();
 
     try {
+      setLoading(true);
       const formDataToSend = new FormData();
       formDataToSend.append("name", formData.name);
       formDataToSend.append("email", formData.email);
@@ -107,115 +110,121 @@ const PublisherProfilePage: React.FC = () => {
         addToast("Failed to update profile.", "error");
       }
     } catch (error) {
-      addToast("Something went wrong!", "error");
       console.error("Error updating profile:", error);
+      addToast("Failed to update profile.", "error");
+    } finally {
+      fetchUserProfile();
+      setLoading(false);
     }
   };
 
   return (
-    <div className="min-h-screen w-full bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100 flex items-center justify-center">
-      <div className="w-full max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
-        <h1 className="text-3xl font-bold text-center mb-10">
-          Company Profile
-        </h1>
-        <form
-          onSubmit={handleSubmit}
-          className="bg-white dark:bg-gray-800 p-8 shadow-md rounded-lg"
-        >
-          <div className="space-y-4">
-            <div className="flex flex-col items-center">
-              <label
-                htmlFor="profile-photo"
-                className="relative w-32 h-32 rounded-full border border-gray-300 dark:border-gray-700 bg-gray-200 dark:bg-gray-800 flex items-center justify-center text-gray-500 dark:text-gray-400 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700 transition"
-              >
-                {preview ? (
-                  <Image
-                    src={preview}
-                    alt="Profile Preview"
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-full"
-                  />
-                ) : formData.profilePicUrl ? (
-                  <Image
-                    src={
-                      typeof formData.profilePicUrl === "string"
-                        ? formData.profilePicUrl
-                        : "/default-profile-pic.png"
-                    }
-                    alt="Profile Picture"
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-full object-cover"
-                  />
-                ) : (
-                  <ProfileIcon />
-                )}
-                <span
-                  className="absolute bottom-1 right-1 w-8 h-8 p-2 border bg-blue-600 border-blue-600 rounded-full text-white transition flex items-center justify-center"
-                  style={{ width: "40px", height: "40px" }}
+    <div>
+      {loading && <Loader isVisible />}
+      <div className="min-h-screen w-full bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100 flex items-center justify-center">
+        <div className="w-full max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+          <h1 className="text-3xl font-bold text-center mb-10">
+            Company Profile
+          </h1>
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white dark:bg-gray-800 p-8 shadow-md rounded-lg"
+          >
+            <div className="space-y-4">
+              <div className="flex flex-col items-center">
+                <label
+                  htmlFor="profile-photo"
+                  className="relative w-32 h-32 rounded-full border border-gray-300 dark:border-gray-700 bg-gray-200 dark:bg-gray-800 flex items-center justify-center text-gray-500 dark:text-gray-400 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700 transition"
                 >
-                  <PencilIcon />
-                </span>
-              </label>
-              <input
-                type="file"
-                id="profile-photo"
-                name="photo"
-                accept="image/*"
-                className="hidden"
-                onChange={handleFileChange}
-              />
-            </div>
+                  {preview ? (
+                    <Image
+                      src={preview}
+                      alt="Profile Preview"
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-full"
+                    />
+                  ) : formData.profilePicUrl ? (
+                    <Image
+                      src={
+                        typeof formData.profilePicUrl === "string"
+                          ? formData.profilePicUrl
+                          : "/default-profile-pic.png"
+                      }
+                      alt="Profile Picture"
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-full object-cover"
+                    />
+                  ) : (
+                    <ProfileIcon />
+                  )}
+                  <span
+                    className="absolute bottom-1 right-1 w-8 h-8 p-2 border bg-blue-600 border-blue-600 rounded-full text-white transition flex items-center justify-center"
+                    style={{ width: "40px", height: "40px" }}
+                  >
+                    <PencilIcon />
+                  </span>
+                </label>
+                <input
+                  type="file"
+                  id="profile-photo"
+                  name="photo"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleFileChange}
+                />
+              </div>
 
-            <div>
-              <label className="block text-sm font-medium mb-1">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleInputChange}
-                required
-                className="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300"
-              />
-            </div>
+              <div>
+                <label className="block text-sm font-medium mb-1">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300"
+                />
+              </div>
 
-            <div>
-              <label className="block text-sm font-medium mb-1">Email</label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-                disabled
-                className="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-200 dark:bg-gray-700 text-gray-500 dark:text-gray-400 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300"
-              />
-            </div>
+              <div>
+                <label className="block text-sm font-medium mb-1">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                  disabled
+                  className="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-200 dark:bg-gray-700 text-gray-500 dark:text-gray-400 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300"
+                />
+              </div>
 
-            <div>
-              <label className="block text-sm font-medium mb-1">
-                Company Name
-              </label>
-              <input
-                type="text"
-                name="companyName"
-                value={formData.companyName}
-                onChange={handleInputChange}
-                required
-                className="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300"
-              />
+              <div>
+                <label className="block text-sm font-medium mb-1">
+                  Company Name
+                </label>
+                <input
+                  type="text"
+                  name="companyName"
+                  value={formData.companyName}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full p-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300"
+                />
+              </div>
             </div>
-          </div>
-          <div className="mt-6 flex justify-end">
-            <button
-              type="submit"
-              className="px-5 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
-            >
-              Save
-            </button>
-          </div>
-        </form>
+            <div className="mt-6 flex justify-end">
+              <button
+                type="submit"
+                className="px-5 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
