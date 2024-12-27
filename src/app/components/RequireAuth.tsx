e841fb58531c22b2b0dda1d0a758a8a92971607b
Yaseen <121yaseen@gmail.com>
2024-12-28 03:12:01 +0530

    fix: On refreshing register, it redirects to login

--- src/app/components/RequireAuth.tsx
+++ src/app/components/RequireAuth.tsx
@@ -3,17 +3,24 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+
 import Loader from "./shared/LoaderComponent";
 
 const RequireAuth = ({ children }: { children: React.ReactNode }) => {
   const { status } = useSession();
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
-    if (status === "unauthenticated") {
+    if (
+      status === "unauthenticated" &&
+      pathname !== "/login" &&
+      pathname !== "/register"
+    ) {
       router.push("/login");
     }
-  }, [status, router]);
+  }, [status, router, pathname]);
 
   if (status === "loading") {
     return <Loader isVisible={true} />;
