e11997a2997e6bc32a0452989aa1b83764e8afaa
Josen Jose <josen.jose@soti.net>
2025-01-02 14:48:41 +0530

    build fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import { signIn } from "next-auth/react";
 import PistahIcon from "@/icons/pistahIcon";
 
